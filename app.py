2a35e5df05f7b764222ae6d96e0bfbfbe0eed2a3
Jxe142 <jevans116@rams.wssu.edu>
2018-10-01 00:40:06 -0400

    Added Code for scraping Data for Combine and NFL stats. And I refactor the code so that all of the web scrapping stuff is in its own file called scarpe.py. Next we need to save these scraped files into CSV files this will be like chaching the files so we don't have to scrape them each time which takes about 10 minutes to get all of the data absuoltely

--- app.py
+++ app.py
@@ -4,103 +4,30 @@ import urllib.request as request, urllib.error as error
 from bs4 import BeautifulSoup
 import glob
 import glob
+from scrape import ScrapeCollegeStats, ScrapeNflDraftData, ScrapeCombineData, getYears
 # from pyspark import SparkConf, SparkContext
 # from pyspark.sql import SparkSession
 # from pyspark.sql import functions as F
 # from pyspark.sql.types import IntegerType
- 
 
-'''
-We have about 40364  nodes in the college data in total 
+def main():
+    print("####### Scrapping College Data #######")
+    years = getYears(1956,2018) # Max range 1956 - 2018 Note add one extra year to end
+    dataTypes = ['Rushing', 'Passing', 'Receiving']
+    RushingPlayersCollege, PassingPlayersCollege, ReceivingPlayersCollege =  ScrapeCollegeStats(years, dataTypes)
 
-TODO:
-    * Remove Kicking and specials team data from the NFL Data Set
-'''
-################################################ METHODS FOR THE College STATS DATA ################################################
-'''
-    * Pulls in the collage data for passing yards 
-    * The years are from 1956 until 2017 TODO may have to drop some years to align with NFL data
-    
-    * Table structure Passing :
-    Rnk | Player Name | School | Conf | Games Played | Cmp (passing) | Att (passing) | Percent (passing) | Yards (passing) | Avg Yards (passing) | 
-    TD (passing) | Interceptins (passing) | Rate (passing) | Att (rushing) | Yds (rushing) | Avg Yards (rushing) | TD (rushing) | Year |
-    
-    * Table structure Rushing :
-    Rnk | Player | School | Conf | Games Played | Att (rushing) | Yds (rushing) | Avg (rushing) | TD (rushing) | Rec (receiving) | Yds (receiving) |
-    Avg (receiving) | TD (receiving) | Plays (scrimmage)| Yds (scrimmage) | Avg (scrimmage) | TD (scrimmage) | Year |
+    print("####### Scrapping NFL Data #######")
+    years = getYears(1937,2018) # Max range 1937 - 2018
+    NFLPlayers = ScrapeNflDraftData(years) 
 
-    * Table structure Receiving :
-    Rnk | Player | School | Conf | Games Played | Rec (receiving) | Yds (receiving) | Avg (receiving) | TD (receiving) |  Att (rushing) | Yds (rushing) | 
-    Avg (rushing) | TD (rushing) | Plays (scrimmage)| Yds (scrimmage) | Avg (scrimmage) | TD (scrimmage) | Year |
+    print("####### Scrapping Combine Data #######")
+    years = getYears(2000,2018) # Max range 2000 - 2018
+    dataTypes = ['Offense', 'Defense', 'Special'] 
+    OffensePlayers, DefensePlayers, SpecailPlayers = ScrapeCombineData(years,["Offense","Defense", "Special"]) 
 
-'''
-def ScrapeCollegeStats(years,dataTypes):
-    RushingPlayers = [] # Holds the data for the running backs
-    PassingPlayers = [] # Holds the data for the QB's
-    ReceivingPlayers = [] # Holds the data for Recviers 
-
-    for year in years:
-        print("Getting data fro " + year + ":")
-        for infoType in dataTypes: 
-            if (infoType == "Rushing"):
-                url = "https://www.sports-reference.com/cfb/years/"+year+"-rushing.html"
-                print("Rushing")
-            elif (infoType == "Passing"):
-                url = "https://www.sports-reference.com/cfb/years/"+year+"-passing.html"
-                print("Passing")
-            elif (infoType == "Receiving"):
-                url = "https://www.sports-reference.com/cfb/years/"+year+"-receiving.html"
-                print("Receiving")
-
-            try:
-                page = request.urlopen(url)
-            except HTTPError as e: 
-                print(e)
-            except URLError:
-                print("The url didn't work the server could be down or the domain no longer exists ")
-            else: 
-                soup = BeautifulSoup(page, 'html.parser')
-
-                # Get the data from the table 
-                if (infoType == "Rushing"):
-                    table = soup.find('table', attrs={'id': 'rushing'})
-                elif (infoType == "Passing"):
-                    table = soup.find('table', attrs={'id': 'passing'})
-                elif (infoType == "Receiving"):
-                    table = soup.find('table', attrs={'id': 'receiving'})
-
-                tableBody = table.find('tbody').select("tr")
-
-                for row in tableBody :
-                    text = row.getText(",")
-                    if(text[0].isdigit()): # Makes sure we don't get any table headers in the data
-                        text += "," + year
-                        text = text.replace(",*,", ",") # cleans the data form something we scrapped 
-                        if (infoType == "Rushing"):
-                            RushingPlayers.append(text)
-                        elif (infoType == "Passing"):
-                            PassingPlayers.append(text)
-                        elif (infoType == "Receiving"):
-                            ReceivingPlayers.append(text)
-
-    print("\n".join(RushingPlayers))
-    print("\n".join(PassingPlayers))
-    print(" \n".join(ReceivingPlayers))
-    totalPlayers = len(RushingPlayers) + len(PassingPlayers)  + len(ReceivingPlayers)
-    print("Total " + str(totalPlayers))
-
-def getYears():
-    years = []
-    for x in range(1956,2018):
-        years.append(str(x))
-    return years
-
-years = getYears()
-dataTypes = ['Rushing', 'Passing', 'Receiving']
-ScrapeCollegeStats(years, dataTypes)
+main()
 
 
-################################################ METHODS FOR THE NFL DATA ################################################
 def getNFLData():
     cleanDraftData = []
     paths = getNFLDraftPaths()
